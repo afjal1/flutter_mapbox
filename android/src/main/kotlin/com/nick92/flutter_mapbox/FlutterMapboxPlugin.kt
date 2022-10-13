e46ec5b5baae12b223aa738d412525c9d5872999
Afjal <afjalru@gmail.com>
2022-10-13 18:01:58 +0530

    Hey

--- android/src/main/kotlin/com/nick92/flutter_mapbox/FlutterMapboxPlugin.kt
+++ android/src/main/kotlin/com/nick92/flutter_mapbox/FlutterMapboxPlugin.kt
@@ -107,30 +107,7 @@ class FlutterMapboxPlugin: FlutterPlugin, MethodCallHandler, EventChannel.Stream
       //   }
       //   builder.show()
       // }
-      val builder = AlertDialog.Builder(this)
-      //set title for alert dialog
-      builder.setTitle("Confirm")
-      //set message for alert dialog
-      builder.setMessage("Do you wish to continue?")
-      builder.setIcon(android.R.drawable.ic_dialog_alert)
-      
-      //performing positive action
-      builder.setPositiveButton("Yes"){dialogInterface, which ->
-          Toast.makeText(applicationContext,"clicked yes",Toast.LENGTH_LONG).show()
-      }
-      //performing cancel action
-      builder.setNeutralButton("Cancel"){dialogInterface , which ->
-          Toast.makeText(applicationContext,"clicked cancel\n operation cancel",Toast.LENGTH_LONG).show()
-      }
-      //performing negative action
-      builder.setNegativeButton("No"){dialogInterface, which ->
-          Toast.makeText(applicationContext,"clicked No",Toast.LENGTH_LONG).show()
-      }
-      // Create the AlertDialog
-      val alertDialog: AlertDialog = builder.create()
-      // Set other dialog properties
-      alertDialog.setCancelable(false)
-      alertDialog.show()
+     
 
 
 
